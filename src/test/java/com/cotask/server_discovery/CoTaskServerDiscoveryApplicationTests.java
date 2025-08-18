f123fdce7ba35aa58c5e251a10d3e0236bc45dbb
namung08 <namung08@gmail.com>
2025-08-18 18:59:16 +0900

    :tada: feat: discovery(eureka) 서버 생성

---
+++ src/test/java/com/cotask/server_discovery/CoTaskServerDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cotask.server_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoTaskServerDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
